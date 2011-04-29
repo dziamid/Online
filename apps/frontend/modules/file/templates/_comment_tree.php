1e0020e62e0441591cb29f43da23b8a720356889
Dziamid <dziamid@gmail.com>
2011-04-29 18:59:36 +0300

    reply level limit feature

--- apps/frontend/modules/file/templates/_comment_tree.php
+++ apps/frontend/modules/file/templates/_comment_tree.php
@@ -1,11 +1,13 @@
-<?php function echoNode($tree) { ?>
+<?php function echoNode($tree, $level=0) { ?>
   <ul class='tree'>
   <?php foreach ($tree as $node): ?>
     <li data-id='<?php echo $node['id'] ?>'>
       <?php echo $node['message'] ?>
-      <span class='link reply'>reply</span>
+      <?php if ($level < sfConfig::get('app_reply_level_limit',5)): ?>
+        <span class='link reply'>reply</span>
+      <?php endif; ?>
       <?php if (count($node['__children']) > 0): ?>
-        <?php echo echoNode($node['__children']) ?>
+        <?php echo echoNode($node['__children'], ++$level) ?>
       <?php endif; ?>
     </li>
   <?php endforeach; ?>       
