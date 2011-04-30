a124761451aaab3cd5b564c60794db9feafa88ef
Dziamid <dziamid@gmail.com>
2011-05-01 02:13:43 +0300

    comments number in list

--- apps/frontend/modules/file/templates/_list_td_actions.php
+++ apps/frontend/modules/file/templates/_list_td_actions.php
@@ -1,6 +1,14 @@
 <td>
   <ul class="sf_admin_td_actions">
-    <li class="sf_admin_action_show"><a href="<?php echo url_for('file_show', $file) ?>"><?php echo __('View comments') ?></a></li>
+    <li class="sf_admin_action_show">
+      <a href="<?php echo url_for('file_show', $file) ?>">
+      <?php if(($count = $file->getComments()->count()) > 0): ?>
+        <?php echo __('View comments (%%count%%)',array('%%count%%'=>$file->getComments()->count())) ?>
+      <?php else: ?>
+        <?php echo __('View comments') ?>
+      <?php endif; ?>
+      </a>
+    </li>  
     <?php echo $helper->linkToEdit($file, array(  'params' =>   array(  ),  'class_suffix' => 'edit',  'label' => 'Edit',)) ?>
     <?php echo $helper->linkToDelete($file, array(  'params' =>   array(  ),  'confirm' => 'Are you sure?',  'class_suffix' => 'delete',  'label' => 'Delete',)) ?>
   </ul>
--- apps/frontend/modules/file/templates/_name.php
+++ apps/frontend/modules/file/templates/_name.php
@@ -1,1 +1,5 @@
-<a href="<?php echo sprintf('/uploads/%s', $file->getName()) ?>" target="_blank"><?php echo $file->getName() ?></a>
+<?php if (file_exists(sfConfig::get('sf_upload_dir').'/'.$file->getName())): ?>
+  <a href="<?php echo sprintf('/uploads/%s', $file->getName()) ?>" target="_blank"><?php echo $file->getName() ?></a>
+<?php else: ?>
+  <?php echo $file->getName() ?>
+<?php endif; ?>
