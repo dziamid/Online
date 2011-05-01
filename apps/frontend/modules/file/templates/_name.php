dd2788f0ef983d9e70db5b6bb41c82c252736f68
Dziamid <dziamid@gmail.com>
2011-05-01 16:52:06 +0300

    added file info in file/show

--- apps/frontend/modules/file/templates/_name.php
+++ apps/frontend/modules/file/templates/_name.php
@@ -1,5 +1,1 @@
-<?php if (file_exists(sfConfig::get('sf_upload_dir').'/'.$file->getName())): ?>
-  <a href="<?php echo sprintf('/uploads/%s', $file->getName()) ?>" target="_blank"><?php echo $file->getName() ?></a>
-<?php else: ?>
-  <span><?php echo $file->getName() ?></span>
-<?php endif; ?>
+<a href="<?php echo url_for('file_show', $file) ?>"><?php echo $file->getName() ?></a>
--- apps/frontend/modules/file/templates/showSuccess.php
+++ apps/frontend/modules/file/templates/showSuccess.php
@@ -1,14 +1,52 @@
 <h1><?php echo __('File "%%name%%"', array('%%name%%' => $file->getName())) ?></h1>
 
-<div id="comments">
-  <?php if ($file->hasComments()): ?>
-    <h3><?php echo __('Comments on this file') ?></h3>
-  <?php else: ?>
-    <h3><?php echo __('No one has commented here yet.') ?></h3>  
-  <?php endif; ?>
+<div id="file">
   
-  <?php include_partial('comment_tree', array('tree'=>$comments)) ?>
+  <div id="info">
+    <table class="info">
+     <tbody>
+       <tr>
+         <th><?php echo __('File name') ?></th>
+         <td>
+          <?php if (file_exists(sfConfig::get('sf_upload_dir').'/'.$file->getName())): ?>
+            <a href="<?php echo sprintf('/uploads/%s', $file->getName()) ?>" target="_blank"><?php echo $file->getName() ?></a>
+          <?php else: ?>
+            <span><?php echo $file->getName() ?></span>
+          <?php endif; ?>
+         </td>
+       </tr>
+       <tr>
+         <th><?php echo __('Uploaded at') ?></th>
+         <td><?php echo $file->getDateTimeObject('created_at')->format('d/m/Y') ?></td>
+       </tr>
+       <tr>
+         <th><?php echo __('File owner') ?></th>
+         <td><?php echo $file->getUser()->getUsername() ?></td>
+       </tr>
+       <tr>
+         <th><?php echo __('File owner browser') ?></th>
+         <td><?php echo $file->getUserBrowser() ?></td>
+       </tr>
+       <tr>
+         <th><?php echo __('File owner IP address') ?></th>
+         <td><?php echo $file->getUserIp() ?></td>
+       </tr>
+     </tbody>
+    </table>      
+  </div>
+
   
-  <h3><?php echo __('Your comment') ?></h3>  
-  <?php include_partial('comment_form', array('form'=>$form, 'file'=>$file)) ?>  
+  <div id="comments">
+    <?php if ($file->hasComments()): ?>
+      <h3><?php echo __('Comments on this file') ?></h3>
+    <?php else: ?>
+      <h3><?php echo __('No one has commented here yet.') ?></h3>  
+    <?php endif; ?>
+    
+    <?php include_partial('comment_tree', array('tree'=>$comments)) ?>
+    
+    <h3><?php echo __('Your comment') ?></h3>  
+    <?php include_partial('comment_form', array('form'=>$form, 'file'=>$file)) ?>  
+  </div>  
+
 </div>
