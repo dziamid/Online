69d82b6dd658e4bac1940e56f2d19cf651f5f749
Dziamid <dziamid@gmail.com>
2011-05-03 13:19:50 +0300

    staging env sign in hint

--- apps/frontend/modules/sfGuardAuth/templates/signinSuccess.php
+++ apps/frontend/modules/sfGuardAuth/templates/signinSuccess.php
@@ -3,3 +3,11 @@
 <h1><?php echo __('Signin') ?></h1>
 
 <?php echo get_partial('sfGuardAuth/signin_form', array('form' => $form)) ?>
+
+<div id="hint">
+  <p>Use the following account to test the application</p>
+  <ul>
+    <li><b>Username:</b> demo</li>
+    <li><b>Password:</b> demo</li>
+  </ul>   
+</div>
